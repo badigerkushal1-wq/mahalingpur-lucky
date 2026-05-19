d23f3b7fae6ae4ab1f8a0b7b8c00689554295fbd
badigerkushal1-wq <badigerkushal1@gmail.com>
2026-05-19 19:51:06 +0530

    Refactor user registration and login functions

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
-function registerUser() {
+function registerUser(){
 
 let name = document.getElementById("name").value;
 let mobile = document.getElementById("mobile").value;
 let password = document.getElementById("password").value;
 
-if(name == "" || mobile == "" || password == "") {
-alert("Fill all fields");
+if(name === "" || mobile === "" || password === ""){
+
+alert("Please fill all fields");
 return;
+
 }
 
-localStorage.setItem("ml_name", name);
-localStorage.setItem("ml_mobile", mobile);
-localStorage.setItem("ml_password", password);
+localStorage.setItem("user_mobile", mobile);
+localStorage.setItem("user_password", password);
 
 alert("Account Created Successfully");
 
@@ -19,42 +20,22 @@ window.location.href = "index.html";
 
 }
 
-function loginUser() {
+function loginUser(){
 
 let mobile = document.getElementById("mobile").value;
 let password = document.getElementById("password").value;
 
-let savedMobile = localStorage.getItem("ml_mobile");
-let savedPassword = localStorage.getItem("ml_password");
+let savedMobile = localStorage.getItem("user_mobile");
+let savedPassword = localStorage.getItem("user_password");
 
-if(mobile == savedMobile && password == savedPassword){
-
-localStorage.setItem("loggedIn", "true");
+if(mobile === savedMobile && password === savedPassword){
 
 window.location.href = "dashboard.html";
 
-} else {
+}else{
 
 alert("Invalid Login Details");
 
 }
-
-}
-
-function checkLogin(){
-
-let status = localStorage.getItem("loggedIn");
-
-if(status != "true"){
-window.location.href = "index.html";
-}
-
-}
-
-function logoutUser(){
-
-localStorage.removeItem("loggedIn");
-
-window.location.href = "index.html";
 
 }
